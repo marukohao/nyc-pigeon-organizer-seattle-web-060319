e171a2f6b0d4091d4cab9fa97f696982a611e658
hao wang <ge_nah@hotmail.com>
2019-05-16 23:41:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  hash = {}
+  data.each do |feature, detail|
+    detail.each do |item, names|
+      names.each do |name|
+        hash[name] = {}
+        if !hash[name].key?(feature) 
+          hash[name][feature] = [item]
+        else 
+          hash[name][feature] << item
+        end
+      end
+    end
+  end
+  hash 
 end
