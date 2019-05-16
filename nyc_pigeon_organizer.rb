6ac30b3080acdc1a1733ade6ac86894db95b301d
hao wang <ge_nah@hotmail.com>
2019-05-16 23:56:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  hash = {}
+  data.each do |feature, detail|
+    detail.each do |item, names|
+      names.each do |name| 
+        hash[name] = {}
+      end
+    end
+  end
+  
+  data.each do |feature, detail|
+    detail.each do |item, names|
+      names.each do |name| 
+        if hash[name].key?(feature) 
+          hash[name][feature] << item.to_s
+        else 
+          hash[name][feature] = [item.to_s]
+        end
+      end
+    end
+  end
+  hash
 end
+
